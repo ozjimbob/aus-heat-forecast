780406d2951c67b7866cd6070a2c814d8b98ca5d
Grant Williamson <grant.williamson@utas.edu.au>
2013-04-09 17:23:53 +1000

    Initial main script skelton commit.

---
+++ main.r
@@ -0,0 +1,15 @@
+library(sp)
+library(maptools)
+library(raster)
+
+# Load raster of 95th percentile average daily temperature
+
+# Load in mean daily temperature for previous 30 days
+
+# Load numerical forecast data
+
+# Interpolate numerical forecast data to a raster
+ 
+# Calculate EHF index raster
+
+# Produce output maps
